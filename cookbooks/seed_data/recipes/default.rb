0cc45ba1fcd64489c59c7d63f23619aa1166d2a2
Paul Dowman <paul@pauldowman.com>
2011-08-08 17:13:34 -0400

    Add recipe to run Rails seed data

---
+++ cookbooks/seed_data/recipes/default.rb
@@ -0,0 +1,17 @@
+execute "rake_db_seed" do
+  command "bundle exec rake db:seed"
+  environment 'RAILS_ENV' => node.app_environment
+  cwd "/srv/app1/current" # TODO determine the app name
+  only_if { node.app_environment != 'production' }
+end
+
+# Make sure this only runs once, remove the role after it runs.
+ruby_block "remove_seed_data_role" do
+  block do
+    if node.role?("run_seed_data")
+      Chef::Log.info("Removing role[run_seed_data]")
+      node.run_list.remove("role[run_seed_data]")
+    end
+  end
+end
+
