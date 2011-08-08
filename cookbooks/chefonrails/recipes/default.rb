592437f6770edafe1d4062974b5610ad9ef9e0f6
Paul Dowman <paul@pauldowman.com>
2011-08-08 16:02:01 -0400

    Fix removing of default user, and upgrade packages on each run

--- cookbooks/chefonrails/recipes/default.rb
+++ cookbooks/chefonrails/recipes/default.rb
@@ -1,12 +1,13 @@
-tz = "America/Toronto"
+tz = "America/Toronto" # TODO this should be an attribute
 execute "set timezone" do
   command "echo '#{tz}' > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata"
   not_if "grep '#{tz}' /etc/timezone"
 end
 
-# execute "apt-get upgrade" do
-#   command "apt-get -quy upgrade"
-# end
+execute "apt-get upgrade" do
+  command "apt-get -quy upgrade"
+  command "apt-get clean"
+end
 
 package "htop" do
   action :install
@@ -17,8 +18,9 @@ file "/etc/gemrc" do
   content YAML.dump({"gem" => "--no-ri --no-rdoc"})
 end
 
+# remove the default user, but not if we're bootstrapping as that user
 user "ubuntu" do
   action :remove
-  not_if "test `whoami` = root"
+  not_if "test `echo $USER` = ubuntu"
 end
 
