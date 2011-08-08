bbc2b78d8c63dc1c9dd1d2cefdf7ebe28e32667d
Paul Dowman <paul@pauldowman.com>
2011-08-07 21:41:55 -0400

    Add mail_monitor cookbook

---
+++ cookbooks/mail_monitor/recipes/default.rb
@@ -0,0 +1,26 @@
+package "logtail" do
+  action :install
+end
+
+gem_package "tlsmail" do
+  action :install
+end
+
+template "/usr/local/bin/emergency_mail_sender" do
+  source "emergency_mail_sender.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+cookbook_file "/usr/local/bin/check_mail_logs" do
+  source "check_mail_logs"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+cron "check_mail_logs" do
+  minute "5,20,35,50" # every 15 minutes, slightly offset from the hour
+  command "/usr/local/bin/check_mail_logs"
+end
