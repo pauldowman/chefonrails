ff72e3a4deeeaf0dc9d875d5957d3863e4f4b93e
Paul Dowman <paul@pauldowman.com>
2011-08-28 20:40:28 -0400

    Add some customizable attributes for God cookbook, and change email settings

---
+++ cookbooks/god/attributes/defaults.rb
@@ -0,0 +1,5 @@
+default[:god][:from_email] = 'god@example.com'
+default[:god][:to_email] = 'root'
+default[:god][:email_delivery_method] = :smtp
+default[:god][:email_server_host] = "localhost"
+default[:god][:email_server_port] = 25
