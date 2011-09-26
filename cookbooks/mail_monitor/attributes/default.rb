0d2bcf4c8cc42184cf1012e8021e0d6c26b9ec12
Paul Dowman <paul@pauldowman.com>
2011-09-26 17:53:17 -0400

    Change admin mail sender defaults

--- cookbooks/mail_monitor/attributes/default.rb
+++ cookbooks/mail_monitor/attributes/default.rb
@@ -1,7 +1,7 @@
-default[:mail_monitor][:from_address]             = "admin_mail_sender@example.com"
-default[:mail_monitor][:to_address]               = "logs@example.com"
-default[:mail_monitor][:smtp_server]              = "smtp.gmail.com"
+default[:mail_monitor][:from_address]             = "admin_mail_sender@gmail.com"
+default[:mail_monitor][:to_address]               = "ops@example.com"
+default[:mail_monitor][:smtp_server]              = "" # smtp.gmail.com
 default[:mail_monitor][:smtp_port]                = 587
 default[:mail_monitor][:smtp_mail_from_domain]    = "example.com"
-default[:mail_monitor][:smtp_account_name]        = "admin_mail_sender@example.com"
+default[:mail_monitor][:smtp_account_name]        = "admin_mail_sender@gmail.com"
 default[:mail_monitor][:smtp_password]            = "sekrit"
