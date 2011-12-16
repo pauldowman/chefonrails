18aa95a71f01c1db4f0d87de771c4a58fb20a51a
Paul Dowman <paul@pauldowman.com>
2011-12-16 12:39:00 -0500

    Add metadata to chefonrails cookbook

---
+++ cookbooks/chefonrails/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Paul Dowman"
+maintainer_email "paul@pauldowman.com"
+license          "All rights reserved"
+description      "Custom cookbooks specific to Chef on Rails"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
