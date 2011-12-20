e9d3a13116bbdf070e13ab3b2754966f93842ed1
Paul Dowman <paul@pauldowman.com>
2011-12-20 11:45:46 -0500

    Add a rake task that runs every 5 minutes

--- cookbooks/application/recipes/cron_jobs.rb
+++ cookbooks/application/recipes/cron_jobs.rb
@@ -3,6 +3,12 @@ app_name = app['id']
 run_as = app['owner']
 command_line = "export PATH=\"/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin\";bash -c 'cd #{app[:deploy_to]}/current && RAILS_ENV=#{node.chef_environment} bundle exec rake -s chefonrails:cron:%s'"
 
+cron "#{app_name} every 5 minutes" do
+  minute "*/5"
+  user run_as  
+  command sprintf(command_line, "every_5_minutes") 
+end
+
 cron "#{app_name} hourly" do
   minute "0"
   user run_as  
