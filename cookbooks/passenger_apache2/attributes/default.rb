1729c8683738d2eab96f6e8b9087eaa20765e29d
Paul Dowman <paul@pauldowman.com>
2012-01-17 16:37:40 -0500

    Reduce passenger min_instances

--- cookbooks/passenger_apache2/attributes/default.rb
+++ cookbooks/passenger_apache2/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:passenger][:version]     = "3.0.7"
 default[:passenger][:max_pool_size] = "6"
-default[:passenger][:min_instances] = "6"
+default[:passenger][:min_instances] = "2"
 default[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
 default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
