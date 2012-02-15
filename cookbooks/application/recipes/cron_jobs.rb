838b3987eec5d3edec20639fd3545916db23fe96
Paul Dowman <paul@pauldowman.com>
2012-02-15 10:00:04 -0500

    Change every_5_minutes rake task to every_15_minutes

--- cookbooks/application/recipes/cron_jobs.rb
+++ cookbooks/application/recipes/cron_jobs.rb
@@ -3,10 +3,10 @@ app_name = app['id']
 run_as = app['owner']
 command_line = "export PATH=\"/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin\";bash -c 'cd #{app[:deploy_to]}/current && RAILS_ENV=#{node.chef_environment} bundle exec rake -s chefonrails:cron:%s'"
 
-cron "#{app_name} every 5 minutes" do
-  minute "*/5"
+cron "#{app_name} every 15 minutes" do
+  minute "*/15"
   user run_as  
-  command sprintf(command_line, "every_5_minutes") 
+  command sprintf(command_line, "every_15_minutes") 
 end
 
 cron "#{app_name} hourly" do
